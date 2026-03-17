049da52c3e6beddb5d479ecd737b17840c540e1f
brpezzolato <bruno.pezzolato@icloud.com>
2026-03-17 00:37:55 -0300

    Versao correta de limpeza de dados

--- sistema/main.py
+++ sistema/main.py
@@ -6,7 +6,7 @@ dados = [
     ("Bruno Pezzolato", [1, 10, "10", ""]),
     ("Ana Lério", [9, 10, 8]),
     ("São Marcos", [1, 10, "10", ""]),
-    ("Bruno Pezzolato2", [9, 10, 8, None, 8]),
+    ("Bruno Pezzolato2", []),
 ]
 
 dadosParaMedia, alunosRetirados = funcao.limpezaDosDados(dados)
--- sistema/processamento.py
+++ sistema/processamento.py
@@ -7,10 +7,13 @@ def limpezaDosDados(dados):
     for cada in dados:
         dadosValidos = True
 
+        if len(cada[1]) == 0:
+            dadosValidos = False
+
         for cadaNota in cada[1]:
             if type(cadaNota) != float and type(cadaNota) != int:
                 dadosValidos = False
-        
+
         if dadosValidos == True:
             alunosParaMedia.append(cada)
         else:
