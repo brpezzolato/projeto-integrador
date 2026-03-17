7260918ff558447eb74875384c55c47f359c2a3d
brpezzolato <bruno.pezzolato@icloud.com>
2026-03-17 11:38:46 -0300

    Funcao de atriuicao de status do estudante finalizada e revisada

--- sistema/processamento.py
+++ sistema/processamento.py
@@ -38,3 +38,28 @@ def calculoMedia(dados):
         dadosComMedia.append(novoObjto)
 
     return dadosComMedia
+
+
+def atribuirStatus(dados):
+    print("Atribuindo status dos alunos...")
+
+    alunosComStatus = []
+    medias = []
+
+    for cada in dados:
+        medias.append(cada[2])
+
+    maior = max(medias)
+
+    for cada in dados:
+        if cada[2] == maior:
+            novoObjto = (cada[0], cada[1], cada[2], "Top Student")
+            alunosComStatus.append(novoObjto)
+        elif cada[2] < 7.0:
+            novoObjto = (cada[0], cada[1], cada[2], "Recuperação")
+            alunosComStatus.append(novoObjto)
+        else:
+            novoObjto = (cada[0], cada[1], cada[2], "Situação Normal")
+            alunosComStatus.append(novoObjto)
+
+    return alunosComStatus
